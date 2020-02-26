8050eee028fe387cd9bc8848b6abdc9290f34142
Akshay Dedaniya <54439564+Dedaniya08@users.noreply.github.com>
2020-02-26 10:05:29 +0530

    Slight changes were made to sample file
    The sample file which was initial taken was then modified for better structure in further analysis.

--- gentosnp.py
+++ gentosnp.py
@@ -31,7 +31,7 @@ def identify(row,columnss):
 	yyy=yyy+1;
 	return yyy;
 
-def Process(filename,output_filename):		
+def Process(filename,Sample_name,output_filename):		
 	File_To_read = pd.read_csv(filename,compression='gzip',header=None,sep=' ',na_filter = True,skip_blank_lines=True)
 	Only_genotype=File_To_read.drop([0,1,2,3,4],axis=1);
 	Only_genotype_modified=Only_genotype.dropna(axis='columns');
@@ -39,8 +39,9 @@ def Process(filename,output_filename):
 	RSID_LIST=list(File_To_read.loc[:,1]);
 	rowss = int(Only_genotype_modified.shape[0]);
 	columnss=int((Only_genotype_modified.shape[1])/3);
-	Sample_ID_LIST = pd.read_csv(Sample_name,header= None,sep='\t')
-	Sample_ID_LIST_modified=Sample_ID_LIST.loc[:,1];	
+	Sample_ID_LIST = pd.read_csv(Sample_name,header= 0,sep=' ')
+	Sample_ID_LIST_modified=Sample_ID_LIST.drop(Sample_ID_LIST.index[0]);
+	Sample_ID_LIST_modified=Sample_ID_LIST_modified['ID_1'];
 	Sample_ID_LIST_modified_as_column=list(Sample_ID_LIST_modified.values.reshape((1,columnss)));
 	final_file=np.zeros((rowss,columnss));
 	for row,jj in zip(Only_genotype_modified_values,range(0,rowss)):
@@ -48,7 +49,7 @@ def Process(filename,output_filename):
 	FINAL = pd.DataFrame(final_file,columns=Sample_ID_LIST_modified_as_column,index=RSID_LIST,dtype=int)
 	FINAL.to_csv(output_filename, sep=' ', mode='a')
  
-Process(filename,output_filename)
+Process(filename,Sample_name,output_filename)
 
 
 print(" The process is done ")
